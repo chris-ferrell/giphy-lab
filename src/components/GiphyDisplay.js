4f5bc72d3b37d0dc816ff3db72e1b4490a59639f
unknown-bytes <unknown.develops@gmail.com>
2023-05-03 23:57:24 -0400

    added

---
+++ src/components/GiphyDisplay.js
@@ -0,0 +1,12 @@
+
+const GiphyDisplay = ({gif}) => {
+    // JSX can only return a single parent but it can have unlimited children
+    const loaded = () => {
+       return( 
+       <div>
+            <h1></h1>
+            <h2></h2>
+        </div>
+       );w
+    }
+}
