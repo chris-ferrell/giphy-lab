a434d9bc941de77f30e819b37b2f4ee8470321db
unknown-bytes <unknown.develops@gmail.com>
2023-05-03 23:55:00 -0400

    Working on API call/fetch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Form from './components/Form';
 
 function App() {
+  const [giphy, setGiphy] = useState(null);
+  const apiKey = 'Wj1emufPvtJQ8BQmbs0OTwoMazWTJ7XB'
+  const getGiphy = async (searchTerm) => {
+    // make fetch req to api endpoint
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&limit=1&offset=0&rating=g&lang=en`
+    )
+    // parse the JSON
+    const data = await response.json()
+    setGiphy(data);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form />
   );
 }
 
